d6f4681b4693c73cc0767f70458975ffcac5a7cb
Manuel Rossard <manuel.rossard@u-bordeaux.fr>
2022-12-01 17:52:31 +0100

    Day1 - en allant jusqu'au bout avec array_map.

--- Day1/v2.php
+++ Day1/v2.php
@@ -1,23 +1,22 @@
 <?php
 
-$subSum = function (array $array, $start, $length): int {
-    return array_sum(array_slice($array, $start, $length));
-};
-
 $input = file('input.txt', FILE_IGNORE_NEW_LINES);
 $sep = array_keys(array: $input, filter_value: '');
-$elves = array_map(
-    callback: function ($end) use ($sep): array {
-        return [$sep[array_search(needle: $end, haystack: $sep) - 1] ?? 0, $end];
-    },
-    array   : $sep);
 
 $calories = array_map(
-    callback: function ($elf) use ($input, $subSum): int {
-        return $subSum($input, $elf[0], $elf[1] - $elf[0]);
+    callback: function ($elf): int {
+        return array_sum($elf);
     },
-    array   : $elves);
+    array   : array_map(
+        callback: function ($end) use ($sep, $input): array {
+            $start = $sep[array_search(needle: $end, haystack: $sep) - 1] ?? 0;
+            return array_slice(
+                array : $input,
+                offset: $start,
+                length: $end - $start);
+        },
+        array   : $sep));
 rsort($calories);
 
 echo 'part 1 : ', $calories[0], PHP_EOL;
-echo 'part 2 : ', $subSum($calories, 0, 3), PHP_EOL;
+echo 'part 2 : ', array_sum(array_slice($calories, 0, 3)), PHP_EOL;
