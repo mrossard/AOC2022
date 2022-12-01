5ac972911506b3142f63872d914f4071b2e27a3e
Manuel Rossard <manuel.rossard@u-bordeaux.fr>
2022-12-01 14:21:39 +0100

    Day1

---
+++ Day1/day1.php
@@ -0,0 +1,23 @@
+<?php
+
+$input = file('input.txt',FILE_IGNORE_NEW_LINES);
+
+$nbElves = 0;
+$calories = [];
+$max = 0; $maxId = -1;
+foreach ($input as $line){
+    if($line == ''){
+        $nbElves++;
+        continue;
+    }
+    $calories[$nbElves] = ($calories[$nbElves] ?? 0) + (int) $line;
+}
+
+sort($calories);
+$top[] = array_pop($calories);
+echo 'part 1 : ', $top[0], PHP_EOL;
+
+$top[] = array_pop($calories);
+$top[] = array_pop($calories);
+
+echo 'part 2 : ', array_sum($top), PHP_EOL;
