dcac0ce7d04ffb9fc1b378e36c003fa1b22eefa1
Manuel Rossard <manuel.rossard@u-bordeaux.fr>
2022-12-02 10:06:16 +0100

    Day2

---
+++ Day2/day2.php
@@ -0,0 +1,40 @@
+<?php
+
+$input = file($argv[1], FILE_IGNORE_NEW_LINES);
+
+function part1(int $opp, int $me): int
+{
+    $diff = $me - ord('X') + ord('A') - $opp;
+    return match ($diff) {
+        -1, 2 => $me - ord('W'),
+        0 => 3 + $me - ord('W'),
+        1, -2 => 6 + $me - ord('W')
+    };
+}
+
+function part2(int $opp, int $result): int
+{
+    $oppval = $opp - ord('A') + 1;
+    return match ($result - ord('X')) {
+        0 => (($oppval + 4) % 3) + 1,
+        1 => $oppval + 3,
+        2 => $oppval % 3 + 7
+    };
+}
+
+function getScores($input, $part): array
+{
+    return array_map(
+        callback: function ($line) use ($part) {
+            [$opponent, $me] = array_map(
+                callback: function ($char) {
+                    return [ord($char)];
+                },
+                array   : explode(' ', $line));
+            return $part($opponent[0], $me[0]);
+        },
+        array   : $input);
+}
+
+echo 'part 1: ', array_sum(getScores($input, part1(...))), PHP_EOL;
+echo 'part 2: ', array_sum(getScores($input, part2(...))), PHP_EOL;
